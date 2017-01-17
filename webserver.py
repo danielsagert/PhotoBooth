d300016f9ecae2a712e12d6cdfdb5469a463814b
Daniel Sagert <daniel.sagert@gmail.com>
2017-01-17 20:48:12 +0100

    Camera video test

--- webserver.py
+++ webserver.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask import jsonify
+from camera import Camera
 import settings
 
 app = Flask(__name__)
@@ -17,6 +18,17 @@ def photo():
    return render_template('photo.html',
                            title='Last photo',
                            text='Not implemented yet')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video')
+def video_feed():
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=settings.PORT)
