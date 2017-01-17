d1cc3838de71801d78a2392a7aecf2af6a6bf747
Daniel Sagert <daniel.sagert@gmail.com>
2017-01-17 21:04:18 +0100

    Camera photo test

--- webserver.py
+++ webserver.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 from flask import jsonify
-from camera import Camera
+from picamera import PiCamera
+
 import settings
 
 app = Flask(__name__)
+cam = PiCamera()
 
 @app.route('/api/')
 def api_index():
@@ -11,24 +13,14 @@ def api_index():
 
 @app.route('/')
 def index():
-   return app.send_static_file('index.html')
+    return app.send_static_file('index.html')
 
 @app.route('/photo')
 def photo():
-   return render_template('photo.html',
+    cam.capture('/home/pi/PhotoBooth/static/photos')
+    return render_template('photo.html',
                            title='Last photo',
                            text='Not implemented yet')
-
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-@app.route('/video')
-def video_feed():
-    return Response(gen(Camera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=settings.PORT)
