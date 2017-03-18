47cb4cc39862b3108f24276e8ad4fadb5d719220
Daniel Sagert <daniel.sagert@gmail.com>
2017-03-19 00:31:53 +0100

    Flask SocketIO test

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ sudo apt-get install python-picamera
 
 ### Python Modules
 ```
-sudo pip install flask, picamera
+sudo pip install flask, flask-socketio, picamera
 ```
 
 ### Install and setup samba
--- WebServer.py
+++ WebServer.py
@@ -3,15 +3,18 @@ import os
 
 from flask import Flask, send_from_directory
 from flask import jsonify
+from flask_socketio import SocketIO, emit
 
 from PhotoBooth import shoot
 from Settings import PORT, ROOT_DIRECTORY
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 
 def start():
     app.run(debug=True, host='0.0.0.0', port=PORT)
+    socketio.run(app)
 
 
 @app.after_request
@@ -48,3 +51,23 @@ def photo(filename):
     print('Deliver photo: ', filename)
 
     return send_from_directory('photos', filename)
+
+
+@socketio.on('my event', namespace='/test')
+def test_message(message):
+    emit('my response', {'data': message['data']})
+
+
+@socketio.on('my broadcast event', namespace='/test')
+def test_message(message):
+    emit('my response', {'data': message['data']}, broadcast=True)
+
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    emit('my response', {'data': 'Connected'})
+
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected')
