e64cbad908a6308a9f2e601fa1b2d7a04bb57897
Daniel Sagert <daniel.sagert@gmail.com>
2017-10-22 18:58:41 +0200

    CherryPy Config

--- webserver.py
+++ webserver.py
@@ -49,15 +49,29 @@ def run_server():
     # Mount the WSGI callable object (app) on the root directory
     cherrypy.tree.graft(app, '/')
 
-    # Set the configuration of the web server
-    cherrypy.config.update({
-        'engine.autoreload_on': True,
-        'log.screen': True,
-        'server.socket_port': PORT,
-        'server.socket_host': '0.0.0.0'
-    })
+    # Unsubscribe the default server
+    cherrypy.server.unsubscribe()
 
-    # Start the CherryPy WSGI web server
+    # Instantiate a new server object
+    server = cherrypy._cpserver.Server()
+
+    # Configure the server object
+    server.socket_host = "0.0.0.0"
+    server.socket_port = PORT
+    server.thread_pool = 30
+
+    # Subscribe this server
+    server.subscribe()
+
+    # Set the configuration of the web server
+    # cherrypy.config.update({
+    #     'engine.autoreload_on': True,
+    #     'log.screen': True,
+    #     'server.socket_port': PORT,
+    #     'server.socket_host': '0.0.0.0'
+    # })
+
+    # Start the server engine (Option 1 *and* 2)
     cherrypy.engine.start()
     cherrypy.engine.block()
 
