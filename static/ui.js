61e7c3c4534d08dfd5d9a8a8d610a306862541cc
Daniel Sagert <daniel.sagert@gmail.com>
2017-10-28 11:19:31 +0200

    Converted class into functions

--- static/ui.js
+++ static/ui.js
@@ -1,55 +1,64 @@
-class UI {
-    constructor() {
-        this.container = document.createElement('div');
-        this.container.id = 'photo-container';
-        document.querySelector('body').appendChild(this.container);
-    }
-
-    setPhotos(self, filenames) {
-        filenames.forEach(function (filename) {
-            let img = document.createElement('img');
-            img.setAttribute('src', '/static/photos/' + filename);
-            img.setAttribute('alt', filename);
-            self.container.appendChild(img);
-        });
-
-        console.log('Photos loaded');
-    }
-
-    update() {
-        if (!this.container.hasChildNodes()) {
-            console.log("Container is empty");
-            this.fetchPhotos();
-            return;
-        }
-
-        fetch('/photos/last')
-            .then(response => response.json())
-            .then((json) => {
-                let remoteFilename = json.filename;
-                let localFilename = this.container.firstChild.alt;
-
-                if (localFilename === remoteFilename) {
-                    console.log("No new file available");
-                    return;
-                }
-
-                console.log('New file available: ' + remoteFilename);
-                this.fetchPhotos();
-            });
-    }
-
-    fetchPhotos() {
-        this.container.innerHTML = '';
-
-        fetch('/photos')
-            .then(response => response.json())
-            .then(json => this.setPhotos(this, json.filenames));
-    }
+function createContainer() {
+    let container = document.createElement('div');
+    container.id = 'photo-container';
+    document.querySelector('body').appendChild(container);
+    return container;
 }
 
-let ui = new UI();
+function getContainer() {
+    let container = document.document.getElementById('photo-container');
 
-setInterval(() => {
-    ui.update();
+    if (container) {
+        return container;
+    }
+
+    return createContainer();
+}
+
+function update() {
+    let container = getContainer();
+
+    if (!container.hasChildNodes()) {
+        console.log("Container is empty");
+        this.fetchPhotos();
+        return;
+    }
+
+    fetch('/photos/last')
+        .then(response => response.json())
+        .then((json) => {
+            let remoteFilename = json.filename;
+            let localFilename = container.firstChild.alt;
+
+            if (localFilename === remoteFilename) {
+                console.log("No new file available");
+                return;
+            }
+
+            console.log('New file available: ' + remoteFilename);
+            this.fetchPhotos(container);
+        });
+}
+
+function fetchPhotos(container) {
+    container.innerHTML = '';
+
+    fetch('/photos')
+        .then(response => response.json())
+        .then(json => this.setPhotos(container, json.filenames));
+}
+
+function setPhotos(container, filenames) {
+    filenames.forEach(function (filename) {
+        let img = document.createElement('img');
+        img.setAttribute('src', '/static/photos/' + filename);
+        img.setAttribute('alt', filename);
+        container.appendChild(img);
+    });
+
+    console.log('Photos loaded');
+}
+
+setInterval(function () {
+    update()
 }, 3000);
