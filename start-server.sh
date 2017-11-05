4d4e2dc7d0d5be02b88f91b89b858f3b61f96d59
Daniel Sagert <daniel.sagert@gmail.com>
2017-11-05 15:07:04 +0100

    Integrate PiFace module

--- start-server.sh
+++ start-server.sh
@@ -3,5 +3,5 @@ sudo pkill gunicorn
 pkill -9 -f control.py
 
 cd /home/pi/PhotoBooth/
-sudo python ./control.py
+sudo python ./control.py &
 sudo gunicorn --bind 0.0.0.0:8000 --workers 10 webserver:app
