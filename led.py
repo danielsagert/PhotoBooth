f6eb5acab77e62dfc1ab4d5b6b491c2cefa7c028
Daniel Sagert <daniel.sagert@gmail.com>
2017-11-06 21:44:04 +0100

    LED integration

--- led.py
+++ led.py
@@ -1,7 +1,9 @@
+import thread
 from time import sleep
 
 import pifacedigitalio as pfio
 
+LED_BUTTON = 0
 piface = pfio.PiFaceDigital()
 _mode = None
 _stop = True
@@ -13,14 +15,21 @@ def set_mode(mode):
 
 
 def on():
+    thread.start_new_thread(loop, ())
+
+
+def loop():
     while _stop:
         if _mode == 'fast':
-            piface.leds[0].turn_on()
+            piface.leds[LED_BUTTON].turn_on()
             sleep(0.5)
-            piface.leds[0].turn_off()
+            piface.leds[LED_BUTTON].turn_off()
             sleep(0.5)
         elif _mode == 'permanent':
-            piface.leds[0].turn_on()
+            piface.leds[LED_BUTTON].turn_on()
+        else:
+            piface.leds[LED_BUTTON].turn_off()
+            break
 
 
 def off():
