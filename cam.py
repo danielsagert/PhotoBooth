5ae25f9c4cca98baf74b3c0ab6f9513889786249
Daniel Sagert <daniel.sagert@gmail.com>
2017-11-09 21:53:21 +0100

    Opaque preview

--- cam.py
+++ cam.py
@@ -33,7 +33,7 @@ def shoot():
 
     with PiCamera() as camera:
         camera.resolution = (2592, 1944)
-        camera.start_preview(alpha=120)
+        camera.start_preview(alpha=200)
         # Camera warm-up time
         sleep(2)
         led.on('permanent')
