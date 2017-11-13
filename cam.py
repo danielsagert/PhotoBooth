f24afa2d6ae5e66a3bed7df961e8d2e7a26e119b
Daniel Sagert <daniel.sagert@gmail.com>
2017-11-13 22:16:26 +0100

    Camera overlay

--- cam.py
+++ cam.py
@@ -3,6 +3,8 @@ import os
 from datetime import datetime
 from time import sleep
 
+from PIL import Image
+
 import led
 
 try:
@@ -34,6 +36,29 @@ def shoot():
     with PiCamera() as camera:
         camera.resolution = (2592, 1944)
         camera.start_preview(alpha=200)
+
+        # Load the arbitrarily sized image
+        img = Image.open('static/photos/photobooth-test.jpg')
+        # Create an image padded to the required size with
+        # mode 'RGB'
+        pad = Image.new('RGB', (
+            ((img.size[0] + 31) // 32) * 32,
+            ((img.size[1] + 15) // 16) * 16,
+        ))
+        # Paste the original image into the padded one
+        pad.paste(img, (0, 0))
+
+        # Add the overlay with the padded image as the source,
+        # but the original image's dimensions
+        o = camera.add_overlay(pad.tobytes(), size=img.size)
+        # By default, the overlay is in layer 0, beneath the
+        # preview (which defaults to layer 2). Here we make
+        # the new overlay semi-transparent, then move it above
+        # the preview
+        o.alpha = 128
+        o.layer = 3
+
+        # camera.start_preview(alpha=200)
         # display.countdown(3)
         # Camera warm-up time
         sleep(2)
