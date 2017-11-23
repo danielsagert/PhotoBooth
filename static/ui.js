88c8b1049a0b4224f4edb8e22de525c2ed77e1e6
Daniel Sagert <daniel.sagert@gmail.com>
2017-11-23 10:53:21 +0100

    Better image loading

--- static/ui.js
+++ static/ui.js
@@ -1,35 +1,37 @@
 const hostname = window.location.hostname;
 const urlFlask = 'http://' + hostname + ':8000';
+const urlLastFilenames = urlFlask + '/photos';
 const urlApache = 'http://' + hostname + ':80';
 const lastFilenames = [];
 
-function loadPhotos() {
-    let container = document.getElementById('photo-container');
-    let url = urlFlask + '/photos';
-
-    fetch(url)
+function update() {
+    fetch(urlLastFilenames)
         .then(response => response.json())
         .then((json) => {
             for (let i = json.filenames.length - 1; i >= 0; i--) {
                 let filename = json.filenames[i];
 
+                // Check if image was already added
                 if (!lastFilenames.includes(filename)) {
+                    // Place new image at first position and add DOM element
                     lastFilenames.splice(0, 0, filename);
                     lastFilenames.splice(6);
-                    addImage(container, filename);
+                    addImage(filename);
                 }
             }
         });
 }
 
-function addImage(container, filename) {
+function addImage(filename) {
     console.log('Add new image to container:' + filename);
 
     let img = new Image();
 
     img.onload = function () {
+        let container = document.getElementById('photo-container');
         container.insertBefore(img, container.firstChild);
 
+        // Only keep 6 images
         while (container.children.length > 6) {
             container.removeChild(container.lastElementChild);
         }
@@ -40,77 +42,6 @@ function addImage(container, filename) {
 }
 
 window.onload = function () {
-    loadPhotos();
-    setInterval(loadPhotos, 2000);
+    update();
+    setInterval(update, 1000);
 };
-//
-//
-// function update() {
-//     let container = document.getElementById('photo-container');
-//
-//     if (!container.hasChildNodes()) {
-//         console.log("Container is empty");
-//         loadPhotos(container);
-//         return;
-//     }
-//
-//     loadNewPhotos(container);
-// }
-//
-// function loadPhotos(container, lastPhoto) {
-//     let url;
-//     if (!lastPhoto) {
-//         console.log('Load all photos');
-//         container.innerHTML = '';
-//         url = 'photos';
-//     } else {
-//         console.log('Load all photos until ' + lastPhoto);
-//         url = urlFlask + '/photos?lastphoto=' + lastPhoto;
-//     }
-//
-//     fetch(url)
-//         .then(response => response.json())
-//         .then((json) => {
-//             for (const filename of json.filenames) {
-//                 let img = new Image();
-//
-//                 img.onload = function () {
-//                     if (lastPhoto) {
-//                         container.insertBefore(img, container.firstChild);
-//                     } else {
-//                         container.appendChild(img);
-//                     }
-//
-//                     while (container.children.length > 6) {
-//                         container.removeChild(container.lastElementChild);
-//                     }
-//                 };
-//
-//                 img.setAttribute('src', urlApache + '/photos/' + filename);
-//                 img.setAttribute('alt', filename);
-//             }
-//
-//             console.log('All photos loaded');
-//         });
-// }
-//
-// function loadNewPhotos(container) {
-//     fetch(urlFlask + '/photos/last')
-//         .then(response => response.json())
-//         .then((json) => {
-//             let remoteFilename = json.filename;
-//             let localFilename = container.firstChild.alt;
-//
-//             if (localFilename === remoteFilename) {
-//                 console.log("No new file available");
-//                 return;
-//             }
-//
-//             loadPhotos(container, localFilename);
-//         });
-// }
-//
-// window.onload = function () {
-//     update();
-//     setInterval(update, 1000);
-// };
