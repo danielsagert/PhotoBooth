ec234971cb48f8e09ea30c480f68a836ff32a6f8
Daniel Sagert <daniel.sagert@gmail.com>
2017-11-26 11:44:10 +0100

    Log into logs directory

--- start-server.sh
+++ start-server.sh
@@ -3,5 +3,8 @@ sudo pkill gunicorn
 pkill -9 -f control.py
 
 cd /home/pi/PhotoBooth/
-sudo python ./control.py &
-sudo gunicorn --bind 0.0.0.0:8000 --workers 10 webserver:app
+
+mkdir -p logs
+
+sudo python ./control.py >logs/control.log 2>&1 &
+sudo gunicorn --bind 0.0.0.0:8000 --workers 10 webserver:app >logs/webserver.log 2>&1
