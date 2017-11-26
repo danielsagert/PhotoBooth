43063e3cc74427ea985ce6cd49ac0c8efedab8d2
Daniel Sagert <daniel.sagert@gmail.com>
2017-11-26 12:12:17 +0100

    Start script optimization

--- start-server.sh
+++ start-server.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 sudo pkill gunicorn
-pkill -9 -f control.py
+sudo pkill -9 -f control.py
 
 cd /home/pi/PhotoBooth/
 
 mkdir -p logs
 
-sudo python ./control.py >> logs/control.log 2>&1 &
-sudo gunicorn --bind 0.0.0.0:8000 --workers 10 webserver:app >> logs/webserver.log 2>&1
+python ./control.py >> logs/control.log 2>&1 &
+gunicorn --bind 0.0.0.0:8000 --workers 10 webserver:app >> logs/webserver.log 2>&1
