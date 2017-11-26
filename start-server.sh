02543b3e7d7933204fe369e7121ad37cd2e08752
Daniel Sagert <daniel.sagert@gmail.com>
2017-11-26 13:50:09 +0100

    Run gunicorn in background

--- start-server.sh
+++ start-server.sh
@@ -7,4 +7,4 @@ cd /home/pi/PhotoBooth/
 mkdir -p logs
 
 python ./control.py >> ./logs/control.log 2>&1 &
-gunicorn --bind 0.0.0.0:8000 --workers 10 webserver:app >> ./logs/webserver.log 2>&1
+gunicorn --bind 0.0.0.0:8000 --workers 10 webserver:app >> ./logs/webserver.log 2>&1 &
