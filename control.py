a8beffef94903d9d80daaa3fd4149f35fa1df2b6
Daniel Sagert <daniel.sagert@gmail.com>
2018-03-17 13:27:50 +0100

    Shutdown system if button 4 is pressed

--- control.py
+++ control.py
@@ -17,7 +17,7 @@ def button1_pressed(event):
 
 
 def button2_pressed(event):
-    log('Button 2 pressed - trigger shutdown...')
+    log('Button 2 pressed - shutdown application...')
     global shutdown
     shutdown = True
 
@@ -27,14 +27,20 @@ def button3_pressed(event):
     os.system('reboot')
 
 
+def button4_pressed(event):
+    log('Button 4 pressed - shutdown system...')
+    os.system('shutdown -h now')
+
+
 def monitor_buttons():
     piface = pfio.PiFaceDigital()
     listener = pfio.InputEventListener(chip=piface)
     listener.register(0, pfio.IODIR_FALLING_EDGE, button1_pressed)
     listener.register(1, pfio.IODIR_FALLING_EDGE, button2_pressed)
     listener.register(2, pfio.IODIR_FALLING_EDGE, button3_pressed)
+    listener.register(3, pfio.IODIR_FALLING_EDGE, button4_pressed)
     listener.activate()
-    log("Button 1 listener activated")
+    log("Button listeners activated")
 
     while not shutdown:
         time.sleep(1)
